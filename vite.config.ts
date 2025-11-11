d49457a4bbfd01b96aab1c2c70d49cd5f1af2d9c
han-tkp <h4nnyza@gmail.com>
2025-11-11 07:48:22 +0700

    Fix: Add commonjsOptions to vite.config.ts for firebase module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,10 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         }
       }
+      build: {
+        commonjsOptions: {
+          include: [/node_modules/],
+        },
+      },
     };
 });
